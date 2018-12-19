0a637648ac7bffbc745067a0bef07258670d7876
bnl <ducounau@gmail.com>
2018-12-19 12:13:05 -0800

    modify format

--- src/main/java/com/svit/java/l5/threadlambda/CyclicBarrierTest.java
+++ src/main/java/com/svit/java/l5/threadlambda/CyclicBarrierTest.java
@@ -35,7 +35,6 @@ public class CyclicBarrierTest {
             try {
                 Thread.sleep(5000);      //以睡眠来模拟写入数据操作
                 System.out.println("线程"+Thread.currentThread().getName()+"写入数据完毕，等待其他线程写入完毕");
-
                 cyclicBarrier.await();
             } catch (InterruptedException e) {
                 e.printStackTrace();
